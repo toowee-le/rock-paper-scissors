458d0b689a2964acbb4846daa7e37705fc760625
Tuyet Le <tuyet.a.le@hotmail.co.uk>
2020-01-04 19:59:28 +0000

    Add a click event listener to each choice

--- app.js
+++ app.js
@@ -5,6 +5,16 @@ const result = document.getElementById('result');
 const restart = document.getElementById('restart');
 
 const scoreboard = {
-    player: 0;
-    computer: 0;
+    player: 0,
+    computer: 0
+}
+
+// Play game
+function play(e) {
+    console.log(e.target.id);
+}
+
+// Event listeners
+for (let i = 0; i < choices.length; i++) {
+    choices[i].addEventListener('click', play);
 }
